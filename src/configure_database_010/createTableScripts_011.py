9c852ac65925e190905db24544304db77f963d41
oleeson <omleeson@gmail.com>
2019-12-12 09:36:29 -0500

    add table script for source table

--- src/configure_database_010/createTableScripts_011.py
+++ src/configure_database_010/createTableScripts_011.py
@@ -20,27 +20,36 @@ smallIntFields = ['integer']
 timeStampFields = ['datetime']
 
 for key, value in endpoints.items():
-    TOKEN = cf.tokenOut()
     tablename = key
     url = value
     options = requests.options(url, headers={'Authorization': TOKEN})
+    fieldList = ["CREATE TABLE",tablename,"("]
     try:
         optionsDict = options.json()['actions']['POST']
+        print('table script for '+tablename+' created')
+        for key, value in optionsDict.items():
+            fieldList.append(key)
+            if value['type'] in textFields:
+                dtype = 'text'
+            elif value['type'] in smallIntFields:
+                dtype = 'smallint'
+            elif value['type'] in timeStampFields:
+                dtype = 'timestamp'
+            else:
+                dtype = value['type']
+            fieldList.append(dtype)
+            fieldList.append(",")
     except:
-        print('no data types found in: '+ tablename)
-    fieldList = ["CREATE TABLE",tablename,"("]
-    for key, value in optionsDict.items():
-        fieldList.append(key)
-        if value['type'] in textFields:
-            dtype = 'text'
-        elif value['type'] in smallIntFields:
-            dtype = 'smallint'
-        elif value['type'] in timeStampFields:
-            dtype = 'timestamp'
-        else:
-            dtype = value['type']
-        fieldList.append(dtype)
-        fieldList.append(",")
+        print('no data types found in: '+ tablename + '... all dtypes will be read in as text.')
+        try:
+            req = requests.get(url, headers={'Authorization': TOKEN})
+            resultsDict = req.json()['results'][0]
+            for key in resultsDict:
+                fieldList.append(key)
+                fieldList.append('text')
+                fieldList.append(',')
+        except:
+            print('not Authorized to return results for '+tablename+" table.")
     fieldList.pop()
     fieldList.append(endScript)
     tableScript = sep.join(fieldList)
